2a774292c71b286d6abffa7033065c89d4ef9800
Jonmorais <jonathan_ms15@hotmail.com>
2022-04-05 19:05:50 -0300

    Created getters and setters

--- students-software/src/students.ts
+++ students-software/src/students.ts
@@ -9,6 +9,35 @@ class Students {
     this._enrollment = enrollment;
     this._grades = []
   }
+
+  public get enrollment(): string {
+    return this._enrollment;
+  }
+
+  
+  public set enrollment(value : string) {
+    this._enrollment = value;
+  }
+  
+  public get name(): string {
+    return this._name;
+  }
+
+  public set name(value: string) {
+    if (value.length < 2) {
+      throw new Error('The name must have at least 2 characters')
+    }
+
+    this._name = value;
+  }
+
+  public get grades() : number[] {
+    return this._grades
+  }
+    
+  public set grades(value : number[]) {
+    this._grades = value;
+  }  
 }
 
 export default Students;
